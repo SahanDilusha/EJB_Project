fb59edcbf3893b76df8bdabfaa2657b74bc57846
Sahan Dilusha <147801944+SahanDilusha@users.noreply.github.com>
2025-06-05 22:02:07 +0530

    update NewBidController

--- src/main/java/org/popcorntech/bidsystem/controller/NewBidController.java
+++ src/main/java/org/popcorntech/bidsystem/controller/NewBidController.java
@@ -3,6 +3,7 @@ package org.popcorntech.bidsystem.controller;
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 import jakarta.ejb.EJB;
+import jakarta.inject.Inject;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -16,8 +17,10 @@ import org.popcorntech.bidsystem.entities.Bid;
 import org.popcorntech.bidsystem.entities.BidStatus;
 import org.popcorntech.bidsystem.entities.Product;
 import org.popcorntech.bidsystem.entities.User;
+import org.popcorntech.bidsystem.service.BidNotificationService;
 
 import java.io.IOException;
+import java.math.BigDecimal;
 
 @WebServlet("/addBid")
 public class NewBidController extends HttpServlet {
@@ -31,6 +34,9 @@ public class NewBidController extends HttpServlet {
     @EJB
     BidServiceBean bidServiceBean;
 
+    @Inject
+    private BidNotificationService bidService;
+
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
@@ -42,15 +48,19 @@ public class NewBidController extends HttpServlet {
 
         try {
 
+            JsonObject bidJson = gson.fromJson(req.getReader(), JsonObject.class);
+
             User user = userServiceBean.getById(26);
             BidStatus bidStatus = bidStatusServiceBean.getProductCategory(3);
-            Product product = productsServiceBean.getById(1);
+            Product product = productsServiceBean.getById(bidJson.get("prodctId").getAsInt());
 
-            Bid bid = bidServiceBean.addBid(user, product, bidStatus, 1000.00);
+            Bid bid = bidServiceBean.addBid(user, product, bidStatus, bidJson.get("price").getAsDouble());
 
             jsonObject.addProperty("status", true);
 
             jsonObject.addProperty("message", bid.getId());
+
+            bidService.notify("Bid on Item " + product.getName() + " with amount $" + BigDecimal.valueOf(bid.getPrice()).setScale(2));
 
         } catch (Exception e) {
             e.printStackTrace();
---
+++ src/main/java/org/popcorntech/bidsystem/listener/BidListener.java
@@ -0,0 +1,27 @@
+package org.popcorntech.bidsystem.listener;
+
+import jakarta.ejb.ActivationConfigProperty;
+import jakarta.ejb.MessageDriven;
+import jakarta.jms.*;
+
+@MessageDriven(
+        activationConfig = {
+                @ActivationConfigProperty(propertyName = "destinationType", propertyValue = "jakarta.jms.Queue"),
+                @ActivationConfigProperty(propertyName = "destinationLookup", propertyValue = "jms/BidQueue")
+        }
+)
+public class BidListener implements MessageListener {
+
+    @Override
+    public void onMessage(Message message) {
+        try {
+            if (message instanceof TextMessage) {
+                String bidUpdate = ((TextMessage) message).getText();
+                System.out.println("JMS Consumer received bid: " + bidUpdate);
+
+            }
+        } catch (JMSException e) {
+            e.printStackTrace();
+        }
+    }
+}
